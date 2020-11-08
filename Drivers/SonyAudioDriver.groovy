ec140aead3a5389def3da7438f1ce987e2a53aa3
jonesalexr <40618310+jonesalexr@users.noreply.github.com>
2020-11-08 12:02:04 -0500

    Update SonyAudioDriver.groovy

--- Drivers/SonyAudioDriver.groovy
+++ Drivers/SonyAudioDriver.groovy
@@ -1,7 +1,7 @@
 /**
  *  Sony Soundbar RestfulAPI
  *  Hubitat Integration
- *  IMPORT URL: https://raw.githubusercontent.com/jonesalexr/hubitat/main/SonyAudioDriver.groovy
+ *  IMPORT URL: https://raw.githubusercontent.com/jonesalexr/hubitat/main/Drivers/SonyAudioDriver.groovy
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -16,47 +16,104 @@
  metadata {
   definition (name: "Sony Soundbar API", namespace: "ajones", author: "Alex Jones") {
     capability "Switch"
-    capability "Refresh"}
+    capability "Refresh"
+    capability "Polling"
+    command "UpdateAll"
+    }
 
 preferences {
-        input("ipAddress", "string", title:"Sony Soundbar IP Address", required:true, displayDuringSetup:true)
-        input("ipPort", "string", title:"Soundbar Port (default: 10000)", defaultValue:10000, required:true, displayDuringSetup:true)
-        input("refreshInterval", "enum", title: "Refresh Interval in minutes", defaultValue: "10", required:true, displayDuringSetup:true, options: ["1","5","10","15","30"])
+        input name: "ip1", type: "number", range: "0..254", required: true, title: "Ip address 1", displayDuringSetup: true
+		    input name: "ip2", type: "number", range: "0..254", required: true, title: "Ip address 2", displayDuringSetup: true
+		    input name: "ip3", type: "number", range: "0..254", required: true, title: "Ip address 3", displayDuringSetup: true
+		    input name: "ip4", type: "number", range: "0..254", required: true, title: "Ip address 4", displayDuringSetup: true
+        input name: "device_port", type: "number", range: "0..99999", defaultValue: "10000", required: true, title: "Device Port", displayDuringSetup: true
+        input("refreshInterval", "enum", title: "Refresh Interval in minutes", defaultValue: "10", required:false, displayDuringSetup:true, options: ["1","5","10","15","30"])
+        input name: "device_psk", type: "text", title: "PSK Passphrase Set on your device", description: "Enter passphrase", required: false, displayDuringSetup: true
+        input"logEnable", "bool", title: "Enable debug logging", defaultValue: true
     }
  }
 
-// Generic Private Functions Not sure what im doing here -------
-private getHostAddress() {
-    def ip = settings.ipAddress
-    def port = settings.ipPort
-    return ip + ":" + port
+// Generic Functions
+
+def updated(){
+	if (logEnable) log.debug("Updated...")
+	unschedule()
+	log.warn "debug logging is: ${logEnable == true}"
+    log.warn "description logging is: ${txtEnable == true}"
+    if (logEnable) runIn(1800,logsOff)
+	state.device_poll_count = 0
+	if (logEnable) log.debug( "Preferences Updated rebuilding IP Address, MAC address and Hex Network ID")
+	ipaddress()
+	iphex()
+	refresh()
+	runEvery1Minute(poll)
 }
 
-private getDNI(String ipAddress, String port){
-    log.debug "Generating DNI"
-    String ipHex = ipAddress.tokenize( '.' ).collect {  String.format( '%02X', it.toInteger() ) }.join()
-    String portHex = String.format( '%04X', port.toInteger() )
-    String newDNI = ipHex + ":" + portHex
-    return newDNI
+def ipaddress(){
+	//Build an IP Address from the 4 input preferences
+	if (logEnable) log.debug( "building IP address from Preferences")
+	state.device_ip = "${ip1}" + "." + "${ip2}" + "." + "${ip3}" + "." + "${ip4}"
+	if (logEnable) log.debug( "IP Address State Value Set to = ${state.device_ip}:${device_port}" )
 }
 
-private apiGet(def apiCommand) {
-    log.debug "Executing hubaction on " + getHostAddress() + apiCommand
-    sendEvent(name: "hubactionMode", value: "local")
+def iphex(){
+	//create a Hex of the IP this will need to be set as the Network ID
+	//TO DO Set the Network IP automatically or Show the user the Value to set manually
+	if (logEnable) log.debug( "Creating Hex of IP: ${state.device_ip}")
 
-    def hubAction = new hubitat.device.HubAction(
-        method: "GET",
-        path: apiCommand,
-        headers: [Host:getHostAddress()]
-    )
+	
+	String deviceipstring = state.device_ip
+	String device_ip_hex = deviceipstring.tokenize( '.' ).collect {
+		String.format( '%02x', it.toInteger() )
+	}.join()
 
-    return hubAction
+	//set the global value of state.ip_hex
+	state.ip_hex = device_ip_hex
+	if (logEnable) log.debug ("IP Hex stored Globaly as '${state.ip_hex}'")
+
+	if (logEnable) log.debug( "Creating Hex of Port: ${device_port}")
+
+
+    String deviceportstring = device_port
+    String device_port_hex = deviceportstring.tokenize( '.' ).collect {
+    	String.format( '%04x', it.toInteger() )
+    }.join()
+
+    //Set the Global Value of state.port_hex
+    state.port_hex = device_port_hex
+    if (logEnable) log.debug ("Port Hex stored Globaly as '${state.port_hex}'")
+
+    if (logEnable) log.debug( "Please set your Device Network ID to the following to allow the state to be captured: ${state.ip_hex}:${state.port_hex}" )
+    String netid = ("${state.ip_hex}:${state.port_hex}")
+    if (logEnable) log.debug( "Netid ${netid}" )
+    //device.deviceNetworkId = ("${netid}")
 }
 
-private sendJsonCommand(json) {
+def parse(description) {
+  //if (logEnable) log.debug ("Parsing '${description}'")
+  def msg = parseLanMessage(description)
+	//Set the Global Value of state.device_mac
+	//if (logEnable) log.debug "${msg}"
+    state.device_mac = msg.mac
+    if (logEnable) log.debug ("MAC Address stored Globally as '${state.device_mac}'")
+    //if (logEnable) log.debug "msg '${msg}'"
+    //if (logEnable) log.debug "msg.json '${msg.json?.id}'"
+    
+  
+  if (msg.json?.id == 2) {
+  	//Set the Global value of state.tv on or off
+    state.device = (msg.json.result[0]?.status == "active") ? "on" : "off"
+    sendEvent(name: "switch", value: state.device)
+    if (logEnable) log.debug "Device is '${state.device}'"
+    state.device_poll_count = 0
+  }
+}
+
+private sendJsonRpcCommand(json) {
   def headers = [:]
-  headers.put("HOST", "${getHostAddress}")
+  headers.put("HOST", "${state.device_ip}:${device_port}")
   headers.put("Content-Type", "application/json")
+  headers.put("X-Auth-PSK", "${device_psk}")
 
   def result = new hubitat.device.HubAction(
     method: 'POST',
@@ -64,24 +121,91 @@ private sendJsonCommand(json) {
     body: json,
     headers: headers
   )
+
   result
+}
+
+def installed() {
+  if (logEnable) log.debug "Executing 'installed'"
+  poll()
+}
+
+def WOLC() {
+    if (logEnable) log.debug "Executing Wake on Lan"
+	def result = new hubitat.device.HubAction (
+  	  	"wake on lan ${state.device_mac}", 
+   		hubitat.device.Protocol.LAN,
+   		null,
+    	[secureCode: "111122223333"]
+	)
+	return result
+}
+
+def logsOff(){
+    log.warn "debug logging disabled..."
+    device.updateSetting("logEnable",[value:"false",type:"bool"])
 }
 
 // Basic button commands
 
-def refresh() {
-    def json = "{\"method\":\"getPowerStatus\",\"version\":\"1.1\",\"params\":[],\"id\":102}"
-  	def result = sendJsonCommand(json)
-}
-
 def on() {
-
-      def json = "{\"method\":\"setPowerStatus\",\"version\":\"1.1\",\"params\":[{\"status\":active}],\"id\":102}"
-  	  def result = sendJsonCommand(json)
+  if (logEnable) log.debug "Executing 'on'"
+  
+  if (state.device == "polling"){
+  	  WOLC()
+        setPowerStatusOn()
+        //poll()
+  } else {
+        setPowerStatusOn()
+        //poll()
+  }
 }
 
 def off() {
+  if (logEnable) log.debug "Executing 'off'"
+    setPowerStatusOff()
+    //poll()
+}
 
-      def json = "{\"method\":\"setPowerStatus\",\"version\":\"1.1\",\"params\":[{\"status\":standby}],\"id\":102}"
-  	  def result = sendJsonCommand(json)
+def refresh() {
+  if (logEnable) log.debug "Executing 'refresh'"
+  poll()
+}
+
+def poll() {
+  //set state.device to 0ff
+  if (logEnable) log.debug "poll count ${state.device_poll_count}"
+  state.device = "polling"
+  state.device_poll_count = (state.device_poll_count + 1)
+  if (state.device_poll_count > 1 ) {
+  	  sendEvent(name: "switch", value: "off")
+  }
+  if (logEnable) log.debug "Executing 'poll'"
+    getPowerStatus()
+}
+
+def UpdateAll() {
+    if (logEnable) log.debug("UpdateAllClicked.....")
+    sendEvent(name: "switch", value: "off")
+}
+
+//API Commands
+
+
+def getPowerStatus() {
+    def lib = '/sony/system'
+    def json = "{\"id\":2,\"method\":\"getPowerStatus\",\"version\":\"1.1\",\"params\":[]}"
+    def result = sendJsonRpcCommand(json)
+}
+
+def setPowerStatusOn() {
+    def lib = '/sony/system'
+    def json = "{\"method\":\"setPowerStatus\",\"version\":\"1.1\",\"params\":[{\"status\":\"active\"}],\"id\":102}"
+    def result = sendJsonRpcCommand(json)    
+}
+
+def setPowerStatusOff() {
+    def lib = '/sony/system'
+    def json = "{\"method\":\"setPowerStatus\",\"version\":\"1.1\",\"params\":[{\"status\":\"off\"}],\"id\":102}"
+    def result = sendJsonRpcCommand(json)
 }
